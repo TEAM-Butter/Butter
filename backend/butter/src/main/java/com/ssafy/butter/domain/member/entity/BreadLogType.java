06a7d151eb94562210185fade4533395d667aae1
5almiakki <jpui9635@gmail.com>
2025-01-22 23:17:39 +0900

    Feat: BreadLogType 엔티티 생성 #S12P11E204-95

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/member/entity/BreadLogType.java
@@ -0,0 +1,25 @@
+package com.ssafy.butter.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class BreadLogType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long breadLogId;
+
+    @Column(length = 50)
+    @NotNull
+    private String name;
+
+}
