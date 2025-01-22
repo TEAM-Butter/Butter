1c89e0170352d3c14c6b4e6b28aeda5a02848865
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-23 00:10:15 +0900

    Remove: 사용하지 않는 import 정리 #S12P11E204-96

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/entity/Crew.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/entity/Crew.java
@@ -4,7 +4,6 @@ import com.ssafy.butter.domain.live.entity.Live;
 import com.ssafy.butter.domain.schedule.entity.Schedule;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
