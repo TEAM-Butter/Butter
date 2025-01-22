c6708fd3aa1f60f360c61db8d4a821dc72da21af
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-22 23:06:13 +0900

    Feat: Live 엔티티 생성 #S12P11E204-96

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/live/entity/Live.java
@@ -0,0 +1,46 @@
+package com.ssafy.butter.domain.live.entity;
+
+import com.ssafy.butter.domain.crew.entity.Crew;
+import jakarta.persistence.Access;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Live {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "live_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crew_id")
+    @NotNull
+    private Crew crew;
+
+    @Column(length = 50)
+    @NotNull
+    private String title;
+
+    @NotNull
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    @Builder
+    public Live(Crew crew, String title, LocalDateTime startDate, LocalDateTime endDate) {
+        this.crew = crew;
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
