373af12fd4ab1f3e31606560a56a70824741b8d5
5almiakki <jpui9635@gmail.com>
2025-01-22 11:09:56 +0900

    Init: 스프링 부트 프로젝트 및 패키지 생성

---
+++ backend/butter/src/test/java/com/ssafy/butter/ButterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.butter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
