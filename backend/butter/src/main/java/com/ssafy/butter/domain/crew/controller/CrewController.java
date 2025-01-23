4b88ccbee27a0362dac01362cccd934920f4e6c1
5almiakki <jpui9635@gmail.com>
2025-01-24 00:04:50 +0900

    Feat: 크루 생성 API 생성 #S12P11E204-106

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/controller/CrewController.java
@@ -0,0 +1,23 @@
+package com.ssafy.butter.domain.crew.controller;
+
+import com.ssafy.butter.domain.crew.dto.request.CrewCreationRequestDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/crew")
+@Slf4j
+public class CrewController {
+
+    @PostMapping
+    public ResponseEntity<?> createCrew(@ModelAttribute CrewCreationRequestDTO crewCreationRequestDTO) {
+        log.info("createCrew: {}", crewCreationRequestDTO);
+        return ResponseEntity.ok(null);
+    }
+}
