e0f1bc22646fb27c040e76830bd409fb5aade179
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-24 07:13:57 +0900

    Refactor: JpaAuditingConfig를 어플리케이션 실행 코드에서 분리 #S12P11E204-103

--- backend/butter/src/main/java/com/ssafy/butter/global/config/JpaAuditingConfig.java
+++ backend/butter/src/main/java/com/ssafy/butter/global/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.butter.config;
+package com.ssafy.butter.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
