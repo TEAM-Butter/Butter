b230e2eac122edca136d4f132ff751ff052004cf
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-23 15:54:06 +0900

    Feat: CrewMember 엔티티 생성 #S12P11E204-95 #S12P11E204-96

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/entity/CrewMember.java
@@ -0,0 +1,36 @@
+package com.ssafy.butter.domain.crew.entity;
+
+import com.ssafy.butter.domain.member.entity.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CrewMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "crew_member_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crew_id")
+    @NotNull
+    private Crew crew;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @NotNull
+    private Member member;
+
+    @Builder
+    public CrewMember(Crew crew, Member member) {
+        this.crew = crew;
+        this.member = member;
+    }
+}
