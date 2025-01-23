c6455d875679055157afe995afb91a3ed1d63f7c
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-24 07:11:41 +0900

    Feat: EmailAuth 관련 리포지토리 생성 #S12P11E204-103

---
+++ backend/butter/src/main/java/com/ssafy/butter/infrastructure/email/repository/EmailAuthRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.ssafy.butter.infrastructure.email.repository;
+
+import com.ssafy.butter.infrastructure.email.entity.EmailAuth;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class EmailAuthRepositoryImpl implements EmailAuthRepository {
+
+    private final EmailAuthJpaRepository emailAuthJpaRepository;
+
+    @Override
+    public Optional<EmailAuth> findByEmail(String email) {
+        return emailAuthJpaRepository.findByEmail(email);
+    }
+
+    @Override
+    public Optional<EmailAuth> findByEmailAndAuthCode(String email, String verifyCode) {
+        return emailAuthJpaRepository.findByEmailAndAuthCode(email, verifyCode);
+    }
+
+    @Override
+    public void deleteByExpireDateTimeBefore(LocalDateTime now) {
+        emailAuthJpaRepository.deleteByExpireDateTimeBefore(now);
+    }
+
+    @Override
+    public void save(EmailAuth emailAuth) {
+        emailAuthJpaRepository.save(emailAuth);
+    }
+}
