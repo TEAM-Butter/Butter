0931db8a32faebbec9a8ce867835faedfca1dd2a
5almiakki <jpui9635@gmail.com>
2025-01-25 17:19:52 +0900

    Feat: 크루 팔로우 DTO 생성 #S12P11E204-106

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/dto/request/CrewFollowRequestDTO.java
@@ -0,0 +1,13 @@
+package com.ssafy.butter.domain.crew.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class CrewFollowRequestDTO {
+
+    private Long crewId;
+}
