2ca4499354db5c5c118f8bf2eac5c1cde09dc7eb
5almiakki <jpui9635@gmail.com>
2025-01-25 22:52:20 +0900

    Feat: 크루 공지사항 수정 API 생성 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/controller/CrewNoticeController.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/controller/CrewNoticeController.java
@@ -2,6 +2,7 @@ package com.ssafy.butter.domain.crew.controller;
 
 import com.ssafy.butter.domain.crew.dto.request.CrewNoticeCreationRequestDTO;
 import com.ssafy.butter.domain.crew.dto.request.CrewNoticeListRequestDTO;
+import com.ssafy.butter.domain.crew.dto.request.CrewNoticeUpdateRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,12 @@ public class CrewNoticeController {
     @GetMapping("/detail/{id}")
     public ResponseEntity<?> getCrewNotice(@PathVariable Long id) {
         log.info("CrewNotice detail: {}", id);
+        return ResponseEntity.ok(null);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateCrewNotice(@RequestBody CrewNoticeUpdateRequestDTO crewNoticeUpdateRequestDTO) {
+        log.info("CrewNotice updateRequestDTO: {}", crewNoticeUpdateRequestDTO);
         return ResponseEntity.ok(null);
     }
 }
