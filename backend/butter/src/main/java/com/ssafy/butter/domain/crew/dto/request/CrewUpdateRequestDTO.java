321cdf941396b4a68f03a7d19663c19be11c451c
5almiakki <jpui9635@gmail.com>
2025-01-25 17:11:28 +0900

    Feat: 크루 수정 DTO 생성 #S12P11E204-106

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/dto/request/CrewUpdateRequestDTO.java
@@ -0,0 +1,19 @@
+package com.ssafy.butter.domain.crew.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class CrewUpdateRequestDTO {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private MultipartFile image;
+    private String promotionUrl;
+    private MultipartFile portfolioVideo;
+}
