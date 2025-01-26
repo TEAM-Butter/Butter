8c2e3fefaaec875956f59510a997eeda51a45f5f
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-27 00:32:19 +0900

    Feat: Member 서비스 컴포넌트 생성 #S12P11E204-103

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/member/service/MemberService.java
@@ -0,0 +1,4 @@
+package com.ssafy.butter.domain.member.service;
+
+public interface MemberService {
+}
---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/member/service/MemberServiceImpl.java
@@ -0,0 +1,7 @@
+package com.ssafy.butter.domain.member.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+}
