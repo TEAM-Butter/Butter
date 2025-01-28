7f7b4026952512c4e4904bd28f32c63fb3497e02
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-29 00:12:06 +0900

    Feat: 엑세스 토큰 재발급 기능 구현 #S12P11E204-103

--- backend/butter/src/main/java/com/ssafy/butter/auth/controller/LoginController.java
+++ backend/butter/src/main/java/com/ssafy/butter/auth/controller/LoginController.java
@@ -4,16 +4,19 @@ import com.ssafy.butter.auth.dto.AuthInfoDTO;
 import com.ssafy.butter.auth.dto.request.LoginRequestDTO;
 import com.ssafy.butter.auth.service.LoginService;
 import com.ssafy.butter.auth.service.RefreshTokenService;
+import com.ssafy.butter.global.token.CurrentUser;
 import com.ssafy.butter.global.token.JwtExtractor;
 import com.ssafy.butter.global.token.JwtManager;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Enumeration;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @RestController
@@ -35,5 +38,30 @@ public class LoginController {
                 .header(HttpHeaders.AUTHORIZATION, "Bearer "+accessToken)
                 .header("refresh-token", "Bearer "+refreshToken)
                 .build();
+    }
+
+    @GetMapping("/reissue")
+    public ResponseEntity<Void> reissueAccessToken(HttpServletRequest request, @CurrentUser AuthInfoDTO authInfoDTO){
+        validateHeaderExists(request);
+
+        Long memberId = authInfoDTO.id();
+        Enumeration<String> headers = request.getHeaders("refresh-token");
+        String refreshToken = jwtExtractor.extract(headers);
+
+        refreshTokenService.matches(refreshToken, memberId);
+
+        String accessToken = jwtManager.createAccessToken(authInfoDTO);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer "+accessToken)
+                .build();
+    }
+
+    private void validateHeaderExists(HttpServletRequest request){
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String refreshTokenHeader = request.getHeader("Refresh-Token");
+        if (Objects.isNull(authorizationHeader) || Objects.isNull(refreshTokenHeader)) {
+            throw new NoSuchElementException();
+        }
     }
 }
