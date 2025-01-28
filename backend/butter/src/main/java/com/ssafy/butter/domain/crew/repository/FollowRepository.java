13274cae268a5d75367b63e66c9b9c6d3cfeca65
5almiakki <jpui9635@gmail.com>
2025-01-29 01:16:40 +0900

    Feat: repository에 팔로우 엔티티 조회 메서드 추가 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/FollowRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/FollowRepository.java
@@ -1,7 +1,13 @@
 package com.ssafy.butter.domain.crew.repository;
 
+import com.ssafy.butter.domain.crew.entity.Crew;
 import com.ssafy.butter.domain.crew.entity.Follow;
+import com.ssafy.butter.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    Optional<Follow> findByCrewAndMember(Crew crew, Member member);
 }
