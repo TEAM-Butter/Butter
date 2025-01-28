19f3274feadc30006b99494ca84c337904125443
5almiakki <jpui9635@gmail.com>
2025-01-29 00:54:17 +0900

    Feat: CrewMember 엔티티 조회 메서드 생성 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/CrewMemberRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/CrewMemberRepository.java
@@ -1,7 +1,13 @@
 package com.ssafy.butter.domain.crew.repository;
 
+import com.ssafy.butter.domain.crew.entity.Crew;
 import com.ssafy.butter.domain.crew.entity.CrewMember;
+import com.ssafy.butter.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CrewMemberRepository extends JpaRepository<CrewMember, Long> {
+
+    Optional<CrewMember> findByCrewAndMember(Crew crew, Member member);
 }
