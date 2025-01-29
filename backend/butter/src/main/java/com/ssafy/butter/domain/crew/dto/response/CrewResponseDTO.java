c7547e44ef7dac32bc817cba24fffdb8aa7a0b72
5almiakki <jpui9635@gmail.com>
2025-01-29 17:07:31 +0900

    Feat: 크루 응답 DTO record로 수정 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/dto/response/CrewResponseDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/dto/response/CrewResponseDTO.java
@@ -4,38 +4,22 @@ import com.ssafy.butter.domain.crew.entity.Crew;
 import com.ssafy.butter.domain.crew.entity.Notice;
 import com.ssafy.butter.domain.live.entity.Live;
 import com.ssafy.butter.domain.schedule.entity.Schedule;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
 
 import java.util.List;
 
-@Builder
-@Getter
-@ToString
-public class CrewResponseDTO {
-
-    private Long id;
-    private List<Schedule> schedules;
-    private List<Notice> notices;
-    private List<Live> lives;
-    private String name;
-    private String description;
-    private String imageUrl;
-    private String promotionUrl;
-    private String portfolioVideoUrl;
+public record CrewResponseDTO(Long id, List<Schedule> schedules, List<Notice> notices, List<Live> lives, String name, String description, String imageUrl, String promotionUrl, String portfolioVideoUrl) {
 
     public static CrewResponseDTO fromEntity(Crew crew) {
-        return builder()
-                .id(crew.getId())
-                .schedules(crew.getSchedules())
-                .notices(crew.getNotices())
-                .lives(crew.getLives())
-                .name(crew.getName())
-                .description(crew.getDescription())
-                .imageUrl(crew.getImageUrl())
-                .promotionUrl(crew.getPromotionUrl())
-                .portfolioVideoUrl(crew.getPortfolioVideoUrl())
-                .build();
+        return new CrewResponseDTO(
+                crew.getId(),
+                crew.getSchedules(),
+                crew.getNotices(),
+                crew.getLives(),
+                crew.getName(),
+                crew.getDescription(),
+                crew.getImageUrl(),
+                crew.getPromotionUrl(),
+                crew.getPortfolioVideoUrl()
+        );
     }
 }
