d920c80cea57f44dec45b71d980ab8ae8484ec60
5almiakki <jpui9635@gmail.com>
2025-01-29 16:55:02 +0900

    Feat: 크루 팔로우 요청 DTO record로 수정 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/dto/request/CrewFollowRequestDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/dto/request/CrewFollowRequestDTO.java
@@ -1,13 +1,4 @@
 package com.ssafy.butter.domain.crew.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
-@AllArgsConstructor
-@Getter
-@ToString
-public class CrewFollowRequestDTO {
-
-    private Long crewId;
+public record CrewFollowRequestDTO(Long crewId) {
 }
