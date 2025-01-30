aa409703bf4b46eb814b561d36837f8647fafcc4
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-31 01:19:41 +0900

    Feat: WebMvcConfig 작성 #S12P11E204-103

---
+++ backend/butter/src/main/java/com/ssafy/butter/global/config/WebMvcConfig.java
@@ -0,0 +1,34 @@
+package com.ssafy.butter.global.config;
+
+
+import com.ssafy.butter.auth.AuthInterceptor;
+import com.ssafy.butter.global.token.AuthenticatedUserArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    private final AuthenticatedUserArgumentResolver argumentResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/v1/auth/login")
+                .excludePathPatterns("/api/v1/member/signup")
+                .excludePathPatterns("/api/v1/email/**");
+        //TODO : 테스트할 때 엔드포인트 확인하기
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(argumentResolver);
+    }
+}
