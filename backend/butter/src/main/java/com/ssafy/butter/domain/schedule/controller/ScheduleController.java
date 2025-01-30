202b0121f127eefbd58472116fbae2579d585fbe
5almiakki <jpui9635@gmail.com>
2025-01-31 00:49:06 +0900

    Feat: 버스킹 일정 생성 API 생성 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/schedule/controller/ScheduleController.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/schedule/controller/ScheduleController.java
@@ -1,7 +1,11 @@
 package com.ssafy.butter.domain.schedule.controller;
 
+import com.ssafy.butter.domain.schedule.dto.request.ScheduleSaveRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/schedule")
 @Slf4j
 public class ScheduleController {
+
+    @PostMapping
+    public ResponseEntity<?> createSchedule(@RequestBody ScheduleSaveRequestDTO scheduleSaveRequestDTO) {
+        log.info("Create schedule: {}", scheduleSaveRequestDTO);
+        return ResponseEntity.ok(null);
+    }
 }
