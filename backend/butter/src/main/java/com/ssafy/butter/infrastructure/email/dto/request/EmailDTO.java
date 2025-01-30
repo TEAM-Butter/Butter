8048f27b161dfd99bfb971f02d14cfed8fd3cc0d
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-30 23:33:30 +0900

    Remove: 사용하지 않는 파일 삭제 #S12P11E204-103

--- backend/butter/src/main/java/com/ssafy/butter/infrastructure/email/dto/request/EmailDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.butter.infrastructure.email.dto.request;
-
-import jakarta.validation.constraints.Pattern;
-
-public record EmailDTO(
-        @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "올바른 이메일 형식이 아닙니다.")
-        String email
-) {
-}
--- backend/butter/src/main/java/com/ssafy/butter/infrastructure/email/dto/response/EmailWithAuthCodeDTO.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.butter.infrastructure.email.dto.response;
-
-public record EmailWithAuthCodeDTO(
-        String email,
-        String authCode
-) {
-}
