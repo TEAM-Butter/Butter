55f8c92d3632e61a61deb1284e45a7288c3352c8
5almiakki <jpui9635@gmail.com>
2025-01-31 00:38:56 +0900

    Feat: 일정 컨트롤러 생성 #S12P11E204-106

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/schedule/controller/ScheduleController.java
@@ -0,0 +1,13 @@
+package com.ssafy.butter.domain.schedule.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/schedule")
+@Slf4j
+public class ScheduleController {
+}
