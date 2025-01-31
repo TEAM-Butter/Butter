4d05c4e87b38dc89af18b21a01fcc022c8073b98
5almiakki <jpui9635@gmail.com>
2025-01-31 22:49:53 +0900

    Feat: 버스킹 일정 repository에 엔티티 저장 메서드 추가 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/schedule/repository/ScheduleRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/schedule/repository/ScheduleRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.butter.domain.schedule.repository;
 
+import com.ssafy.butter.domain.schedule.entity.Schedule;
+
 public interface ScheduleRepository {
 
-
+    Schedule save(Schedule schedule);
 }
--- backend/butter/src/main/java/com/ssafy/butter/domain/schedule/repository/ScheduleRepositoryImpl.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/schedule/repository/ScheduleRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.butter.domain.schedule.repository;
 
+import com.ssafy.butter.domain.schedule.entity.Schedule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,9 @@ import org.springframework.stereotype.Repository;
 public class ScheduleRepositoryImpl implements ScheduleRepository {
 
     private final ScheduleJpaRepository scheduleJpaRepository;
+
+    @Override
+    public Schedule save(Schedule schedule) {
+        return scheduleJpaRepository.save(schedule);
+    }
 }
