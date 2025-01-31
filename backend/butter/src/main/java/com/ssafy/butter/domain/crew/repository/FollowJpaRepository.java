cb2e54d2214e8b228c9486f2fb8f324c820ecec4
5almiakki <jpui9635@gmail.com>
2025-01-31 22:12:09 +0900

    Refactor: 크루 팔로우 repository 분리 #S12P11E204-106
    Repository와 JpaRepository로 분리

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/FollowJpaRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/FollowJpaRepository.java
@@ -3,14 +3,11 @@ package com.ssafy.butter.domain.crew.repository;
 import com.ssafy.butter.domain.crew.entity.Crew;
 import com.ssafy.butter.domain.crew.entity.Follow;
 import com.ssafy.butter.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface FollowRepository {
-
-    Follow save(Follow follow);
-
-    void delete(Follow follow);
+public interface FollowJpaRepository extends JpaRepository<Follow, Long> {
 
     Optional<Follow> findByCrewAndMember(Crew crew, Member member);
 }
