c3536f5c6ecde3f1ad8a744437c695f2538e1c4e
5almiakki <jpui9635@gmail.com>
2025-01-31 22:28:55 +0900

    Feat: 버스킹 일정 응답 DTO 생성 #S12P11E204-106

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/schedule/dto/response/ScheduleResponseDTO.java
@@ -0,0 +1,23 @@
+package com.ssafy.butter.domain.schedule.dto.response;
+
+import com.ssafy.butter.domain.crew.dto.response.CrewResponseDTO;
+import com.ssafy.butter.domain.schedule.entity.Schedule;
+
+import java.time.LocalDateTime;
+
+public record ScheduleResponseDTO(Long id, CrewResponseDTO crew, String title, String content, String place, Double latitude, Double longitude, LocalDateTime createDate, LocalDateTime updateDate) {
+
+    public ScheduleResponseDTO fromEntity(Schedule schedule) {
+        return new ScheduleResponseDTO(
+                schedule.getId(),
+                CrewResponseDTO.fromEntity(schedule.getCrew()),
+                schedule.getTitle(),
+                schedule.getContent(),
+                schedule.getPlace(),
+                schedule.getLatitude(),
+                schedule.getLongitude(),
+                schedule.getCreateDate(),
+                schedule.getUpdateDate()
+        );
+    }
+}
