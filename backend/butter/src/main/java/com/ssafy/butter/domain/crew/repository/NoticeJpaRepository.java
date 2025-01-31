14ffbd47cd02dc34009dc29f38845209c7f9f607
5almiakki <jpui9635@gmail.com>
2025-01-31 22:17:14 +0900

    Refactor: 크루 공지사항 repository 분리 #S12P11E204-106
    Repository와 JpaRepository로 분리

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/NoticeJpaRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/NoticeJpaRepository.java
@@ -2,17 +2,11 @@ package com.ssafy.butter.domain.crew.repository;
 
 import com.ssafy.butter.domain.crew.entity.Notice;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface NoticeRepository {
-
-    Notice save(Notice notice);
-
-    Optional<Notice> findById(Long id);
-
-    void delete(Notice notice);
+public interface NoticeJpaRepository extends JpaRepository<Notice, Long> {
 
     List<Notice> findAllByOrderByIdDesc(Pageable pageable);
 
