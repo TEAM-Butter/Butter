9455351e5a5d19aacbdf4688976e44f282c4f51d
5almiakki <jpui9635@gmail.com>
2025-02-02 02:55:02 +0900

    Fix: 라이브 목록 조회 요청 필드명 변경 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/live/dto/request/LiveListRequestDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/live/dto/request/LiveListRequestDTO.java
@@ -1,4 +1,4 @@
 package com.ssafy.butter.domain.live.dto.request;
 
-public record LiveListRequestDTO(Long id, Integer pageSize, Integer searchType) {
+public record LiveListRequestDTO(Long liveId, Integer pageSize, Integer searchType) {
 }
