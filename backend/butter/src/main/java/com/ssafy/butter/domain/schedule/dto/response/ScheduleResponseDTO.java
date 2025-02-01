96bdb36bc1ef9995ae3f98adf05831e48bafed08
5almiakki <jpui9635@gmail.com>
2025-02-02 02:22:08 +0900

    Feat: 스케줄 DTO 내 크루 DTO 이름 변경 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/schedule/dto/response/ScheduleResponseDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/schedule/dto/response/ScheduleResponseDTO.java
@@ -1,15 +1,16 @@
 package com.ssafy.butter.domain.schedule.dto.response;
 
+import com.ssafy.butter.domain.crew.entity.Crew;
 import com.ssafy.butter.domain.schedule.entity.Schedule;
 
 import java.time.LocalDateTime;
 
-public record ScheduleResponseDTO(Long id, Crew crew, String title, String content, String place, LocalDateTime buskingDate, Double latitude, Double longitude, LocalDateTime createDate, LocalDateTime updateDate) {
+public record ScheduleResponseDTO(Long id, CrewDTO crew, String title, String content, String place, LocalDateTime buskingDate, Double latitude, Double longitude, LocalDateTime createDate, LocalDateTime updateDate) {
 
     public static ScheduleResponseDTO fromEntity(Schedule schedule) {
         return new ScheduleResponseDTO(
                 schedule.getId(),
-                Crew.fromEntity(schedule.getCrew()),
+                CrewDTO.fromEntity(schedule.getCrew()),
                 schedule.getTitle(),
                 schedule.getContent(),
                 schedule.getPlace(),
@@ -21,10 +22,10 @@ public record ScheduleResponseDTO(Long id, Crew crew, String title, String conte
         );
     }
 
-    public record Crew(Long id, String name, String description, String imageUrl, String promotionUrl) {
+    public record CrewDTO(Long id, String name, String description, String imageUrl, String promotionUrl) {
 
-        public static Crew fromEntity(com.ssafy.butter.domain.crew.entity.Crew crew) {
-            return new Crew(
+        public static CrewDTO fromEntity(Crew crew) {
+            return new CrewDTO(
                     crew.getId(),
                     crew.getName(),
                     crew.getDescription(),
