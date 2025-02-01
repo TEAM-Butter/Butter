ab9e9f9a6d1029bb89bff58f2831ea685f60efcd
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-02 03:55:08 +0900

    Fix: 쿼리 메서드를 통해 String 파라미터가 Email vo와 매치되지 않는 문제 해결 #S12P11E204-113

--- backend/butter/src/main/java/com/ssafy/butter/domain/member/repository/MemberJpaRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/member/repository/MemberJpaRepository.java
@@ -1,11 +1,14 @@
 package com.ssafy.butter.domain.member.repository;
 
 import com.ssafy.butter.domain.member.entity.Member;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(Long memberId);
 
-    Optional<Member> findByEmail(String email);
+    @Query("SELECT m FROM Member m WHERE m.email.value = :email")
+    Optional<Member> findByEmail(@Param("email") String email);
 }
