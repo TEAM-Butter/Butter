c9f6184b27fbb89b7af7d0e734e20a7cd676ca79
5almiakki <jpui9635@gmail.com>
2025-02-02 01:25:26 +0900

    Feat: 라이브 생성 API 생성 #S12P11E204-106

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/live/controller/LiveController.java
@@ -0,0 +1,22 @@
+package com.ssafy.butter.domain.live.controller;
+
+import com.ssafy.butter.domain.live.dto.request.LiveSaveRequestDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/live")
+@Slf4j
+public class LiveController {
+
+    @PostMapping
+    public ResponseEntity<?> createLive(LiveSaveRequestDTO liveSaveRequestDTO) {
+        log.info("Create live request: {}", liveSaveRequestDTO);
+        return ResponseEntity.ok(null);
+    }
+}
