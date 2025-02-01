6572bdc107194f75558761d9b6e05f5cee3489bd
5almiakki <jpui9635@gmail.com>
2025-02-02 02:14:23 +0900

    Feat: 라이브 repository에 엔티티 저장 메서드 추가 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/live/repository/LiveRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/live/repository/LiveRepository.java
@@ -1,4 +1,8 @@
 package com.ssafy.butter.domain.live.repository;
 
+import com.ssafy.butter.domain.live.entity.Live;
+
 public interface LiveRepository {
+
+    Live save(Live live);
 }
--- backend/butter/src/main/java/com/ssafy/butter/domain/live/repository/LiveRepositoryImpl.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/live/repository/LiveRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.butter.domain.live.repository;
 
+import com.ssafy.butter.domain.live.entity.Live;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,9 @@ import org.springframework.stereotype.Repository;
 public class LiveRepositoryImpl implements LiveRepository {
 
     private final LiveJpaRepository liveJpaRepository;
+
+    @Override
+    public Live save(Live live) {
+        return liveJpaRepository.save(live);
+    }
 }
