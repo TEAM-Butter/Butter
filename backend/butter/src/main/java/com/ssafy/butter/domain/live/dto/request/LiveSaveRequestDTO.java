67b577f4d5114fd771be31f3786ee49da4a1dff8
5almiakki <jpui9635@gmail.com>
2025-02-02 02:33:02 +0900

    Fix: 라이브 저장 요청 DTO 필드명 수정 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/live/dto/request/LiveSaveRequestDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/live/dto/request/LiveSaveRequestDTO.java
@@ -2,5 +2,5 @@ package com.ssafy.butter.domain.live.dto.request;
 
 import java.time.LocalDateTime;
 
-public record LiveSaveRequestDTO(Long crewId, String title, LocalDateTime startTime) {
+public record LiveSaveRequestDTO(Long crewId, String title, LocalDateTime startDate) {
 }
