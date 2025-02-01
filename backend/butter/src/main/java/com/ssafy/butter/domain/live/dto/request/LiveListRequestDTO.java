12ffdbae6577bfecb652f87ef16b89f77de6e8f8
5almiakki <jpui9635@gmail.com>
2025-02-02 02:48:02 +0900

    Feat: 라이브 목록 조회 요청 DTO에 페이지 크기 파라미터 추가 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/live/dto/request/LiveListRequestDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/live/dto/request/LiveListRequestDTO.java
@@ -1,4 +1,4 @@
 package com.ssafy.butter.domain.live.dto.request;
 
-public record LiveListRequestDTO(Long id, Integer searchType) {
+public record LiveListRequestDTO(Long id, Integer pageSize, Integer searchType) {
 }
