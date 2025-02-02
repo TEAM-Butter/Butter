724510173293a7a04fd281997a9bcbb31f70e7e9
5almiakki <jpui9635@gmail.com>
2025-02-02 17:14:07 +0900

    Feat: 클립 좋아요 추가 메서드 repository에 추가 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/LikedClipRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/LikedClipRepository.java
@@ -1,4 +1,8 @@
 package com.ssafy.butter.domain.clip.repository;
 
+import com.ssafy.butter.domain.clip.entity.LikedClip;
+
 public interface LikedClipRepository {
+
+    LikedClip save(LikedClip clip);
 }
--- backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/LikedClipRepositoryImpl.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/LikedClipRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.butter.domain.clip.repository;
 
+import com.ssafy.butter.domain.clip.entity.LikedClip;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,9 @@ import org.springframework.stereotype.Repository;
 public class LikedClipRepositoryImpl implements LikedClipRepository {
 
     private final LikedClipJpaRepository likedClipJpaRepository;
+
+    @Override
+    public LikedClip save(LikedClip clip) {
+        return likedClipJpaRepository.save(clip);
+    }
 }
