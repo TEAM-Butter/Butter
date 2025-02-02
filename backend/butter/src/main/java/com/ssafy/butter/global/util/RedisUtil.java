c1ba3d31f38a6e1dddf583481d126c3dbf41c34f
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-02 19:04:19 +0900

    Feat: Redis 컴포넌트 생성 #S12P11E204-113

---
+++ backend/butter/src/main/java/com/ssafy/butter/global/util/RedisUtil.java
@@ -0,0 +1,31 @@
+package com.ssafy.butter.global.util;
+
+import java.time.Duration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisUtil {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public RedisUtil(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofMillis(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+}
