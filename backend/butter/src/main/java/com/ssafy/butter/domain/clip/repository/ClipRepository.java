1b9ce7c621578c53389fcb42b3151845db0f7e87
5almiakki <jpui9635@gmail.com>
2025-02-02 16:32:38 +0900

    Feat: 클립 엔티티 저장 메서드 repository에 추가 #S12P11E204-106

--- backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/ClipRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/ClipRepository.java
@@ -1,4 +1,8 @@
 package com.ssafy.butter.domain.clip.repository;
 
+import com.ssafy.butter.domain.clip.entity.Clip;
+
 public interface ClipRepository {
+
+    Clip save(Clip clip);
 }
--- backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/ClipRepositoryImpl.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/ClipRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.butter.domain.clip.repository;
 
+import com.ssafy.butter.domain.clip.entity.Clip;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,9 @@ import org.springframework.stereotype.Repository;
 public class ClipRepositoryImpl implements ClipRepository {
 
     private final ClipJpaRepository clipJpaRepository;
+
+    @Override
+    public Clip save(Clip clip) {
+        return clipJpaRepository.save(clip);
+    }
 }
