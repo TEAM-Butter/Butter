761a1b5e145bf9f37703f809ebf6a821e23955a0
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-02 23:16:16 +0900

    Feat: Email 비즈니스 로직에 사용되는 상수 클래스 생성 #S12P11E204-113

--- backend/butter/src/main/java/com/ssafy/butter/domain/member/service/MemberService.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/member/service/MemberService.java
@@ -3,9 +3,10 @@ package com.ssafy.butter.domain.member.service;
 import com.ssafy.butter.domain.member.entity.Member;
 import com.ssafy.butter.domain.member.dto.request.SignUpDTO;
 import com.ssafy.butter.domain.member.dto.response.MyPageResponseDTO;
-import com.ssafy.butter.domain.member.entity.Member;
-import com.ssafy.butter.infrastructure.emailAuth.dto.request.EmailDTO;
+import com.ssafy.butter.infrastructure.email.dto.request.SendEmailDTO;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.apache.coyote.BadRequestException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberService {
@@ -13,5 +14,5 @@ public interface MemberService {
     Member findById(Long id);
     Member signUp(SignUpDTO signUpDTO, MultipartFile profileImage);
     MyPageResponseDTO getMyPageInfo(final Long memberId) throws BadRequestException;
-    boolean checkIfEmailExists(EmailDTO emailDTO);
+    boolean checkIfEmailExists(SendEmailDTO emailDTO);
 }
