11dceda4c6f132f8876845a5dcaff2243c5c3b32
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-02 16:41:24 +0900

    Chore: 사용하지 않는 import문 삭제 #S12P11E204-113

--- backend/butter/src/main/java/com/ssafy/butter/auth/controller/LoginController.java
+++ backend/butter/src/main/java/com/ssafy/butter/auth/controller/LoginController.java
@@ -12,16 +12,18 @@ import com.ssafy.butter.global.token.CurrentUser;
 import com.ssafy.butter.global.token.JwtExtractor;
 import com.ssafy.butter.global.token.JwtManager;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
+import java.util.Enumeration;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Enumeration;
-import java.util.NoSuchElementException;
-import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
