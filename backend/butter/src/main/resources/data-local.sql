8003330fe318ab45b962c22fedf155514d30ff71
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-03 00:01:11 +0900

    Feat: 로컬 개발 환경에서 어플리케이션 실행 시 member_type 테이블에 데이터 INSERT 실행되도록 설정 #S12P11E204-113

---
+++ backend/butter/src/main/resources/data-local.sql
@@ -0,0 +1,7 @@
+
+use butter;
+
+-- member_type
+INSERT INTO member_type (member_type_id, name) VALUES (1, 'USER');
+INSERT INTO member_type (member_type_id, name) VALUES (2, 'CREW_LEADER');
+INSERT INTO member_type (member_type_id, name) VALUES (3, 'CREW_MEMBER');
