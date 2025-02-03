232caea1747b6d7e6e3633a3e059adcf16d9f8bc
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-04 00:49:56 +0900

    Feat: 사용자 추가 정보 등록 비즈니스 로직 작성 및 코드 리팩토링 #S12P11E204-113

--- backend/butter/src/main/java/com/ssafy/butter/domain/member/service/MemberServiceImpl.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/member/service/MemberServiceImpl.java
@@ -1,30 +1,35 @@
 package com.ssafy.butter.domain.member.service;
 
 import com.ssafy.butter.auth.dto.AuthInfoDTO;
+import com.ssafy.butter.domain.crew.entity.Genre;
+import com.ssafy.butter.domain.crew.repository.genre.GenreRepository;
+import com.ssafy.butter.domain.member.dto.request.ExtraInfoDTO;
 import com.ssafy.butter.domain.member.dto.request.PasswordUpdateRequestDTO;
+import com.ssafy.butter.domain.member.dto.request.ProfileUpdateRequestDTO;
+import com.ssafy.butter.domain.member.dto.request.SignUpDTO;
 import com.ssafy.butter.domain.member.dto.response.PasswordUpdateResponseDTO;
 import com.ssafy.butter.domain.member.dto.response.ProfileUpdateResponseDTO;
-import com.ssafy.butter.domain.member.dto.request.ProfileUpdateRequestDTO;
+import com.ssafy.butter.domain.member.dto.response.RegisterExtraInfoResponseDTO;
+import com.ssafy.butter.domain.member.dto.response.UserProfileResponseDTO;
+import com.ssafy.butter.domain.member.entity.AvatarType;
 import com.ssafy.butter.domain.member.entity.Member;
 import com.ssafy.butter.domain.member.enums.Gender;
-import com.ssafy.butter.domain.member.repository.MemberRepository;
-import com.ssafy.butter.global.token.JwtManager;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.ssafy.butter.domain.member.dto.request.SignUpDTO;
-import com.ssafy.butter.domain.member.dto.response.MyPageResponseDTO;
+import com.ssafy.butter.domain.member.repository.avatarType.AvatarTypeRepository;
+import com.ssafy.butter.domain.member.repository.member.MemberRepository;
 import com.ssafy.butter.domain.member.vo.BirthDate;
 import com.ssafy.butter.domain.member.vo.Email;
+import com.ssafy.butter.domain.member.vo.Nickname;
 import com.ssafy.butter.domain.member.vo.Password;
+import com.ssafy.butter.global.token.JwtManager;
 import com.ssafy.butter.global.util.encrypt.EncryptUtils;
 import com.ssafy.butter.infrastructure.awsS3.ImageUploader;
 import com.ssafy.butter.infrastructure.email.dto.request.SendEmailDTO;
-
+import java.util.List;
 import java.util.Optional;
-
+import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -34,6 +39,8 @@ public class MemberServiceImpl implements MemberService{
     private final TransactionalMemberService transactionalMemberService;
     private final JwtManager jwtManager;
     private final MemberRepository memberRepository;
+    private final GenreRepository genreRepository;
+    private final AvatarTypeRepository avatarTypeRepository;
     private final EncryptUtils encryptUtils;
     private final ImageUploader imageUploader;
 
@@ -84,24 +91,11 @@ public class MemberServiceImpl implements MemberService{
      */
     @Override
     @Transactional(readOnly = true)
-    public MyPageResponseDTO getMyPageInfo(final Long memberId) throws BadRequestException {
+    public UserProfileResponseDTO getMyProfile(final Long memberId) throws BadRequestException {
         final Member findMember = memberRepository.findById(memberId)
                 .orElseThrow(() -> new BadRequestException("ERR : 멤버를 찾을 수 없습니다"));
 
-        return MyPageResponseDTO.builder()
-                .member(findMember)
-                .build();
-    }
-
-    /**
-     * 파라미터 이메일과 동일한 닉네임을 가진 멤버의 존재 여부를 반환한다
-     * @param nickname 닉네임
-     * @return 동일 닉네임을 가지는 멤버의 존재 여부
-     */
-    @Override
-    public boolean checkIfNickNameExists(String nickname) {
-        Optional<Member> findMember = memberRepository.findByEmail(nickname);
-        return findMember.isPresent();
+        return UserProfileResponseDTO.from(findMember);
     }
 
     /**
@@ -113,8 +107,7 @@ public class MemberServiceImpl implements MemberService{
      */
     @Override
     public ProfileUpdateResponseDTO updateProfile(ProfileUpdateRequestDTO profileUpdateRequestDTO, Long memberId) {
-        Member findMember = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("ERR : 존재하지 않는 회원입니다."));
+        Member findMember = getMember(memberId);
 
         String imageUrl = insertProfileImage(profileUpdateRequestDTO.profileImage());
 
@@ -124,8 +117,7 @@ public class MemberServiceImpl implements MemberService{
     @Override
     @Transactional
     public PasswordUpdateResponseDTO updatePassword(PasswordUpdateRequestDTO passwordUpdateRequestDTO, AuthInfoDTO authInfoDTO) {
-        Member findMember = memberRepository.findById(authInfoDTO.id())
-                .orElseThrow(() -> new IllegalArgumentException("ERR : 존재하지 않는 회원입니다."));
+        Member findMember = getMember(authInfoDTO.id());
 
         if(!findMember.getPassword().match(encryptUtils, passwordUpdateRequestDTO.currentPassword())){
             throw new IllegalStateException("ERR : 현재 비밀 번호가 일치하지 않습니다");
@@ -140,6 +132,21 @@ public class MemberServiceImpl implements MemberService{
         return new PasswordUpdateResponseDTO(accessToken, refreshToken);
     }
 
+    @Override
+    public RegisterExtraInfoResponseDTO saveExtraUserInfo(ExtraInfoDTO extraInfoDTO, Long memberId) {
+        Member findMember = getMember(memberId);
+
+        validateNicknameDuplication(extraInfoDTO.nickname());
+
+        List<Genre> validGenres = getValidGenres(extraInfoDTO.genres());
+
+        AvatarType avatarType = getAvatarType(extraInfoDTO.avatarType());
+
+        findMember.saveExtraInfo(new Nickname(extraInfoDTO.nickname()), extraInfoDTO.profileImage(), avatarType, validGenres);
+
+        return RegisterExtraInfoResponseDTO.from(findMember);
+    }
+
     /**
      * 파라미터 이메일과 동일한 이메일로 가입한 멤버의 존재 여부를 반환한다
      * @param emailDTO 이메일 DTO
@@ -149,6 +156,12 @@ public class MemberServiceImpl implements MemberService{
     public boolean checkIfEmailExists(SendEmailDTO emailDTO) {
         Optional<Member> findMember = memberRepository.findByEmail(emailDTO.email());
         return findMember.isPresent();
+    }
+
+    private Member getMember(Long memberId) {
+        Member findMember = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("ERR : 존재하지 않는 회원입니다."));
+        return findMember;
     }
 
     /**
@@ -172,5 +185,23 @@ public class MemberServiceImpl implements MemberService{
             return imageUploader.uploadImage(profileImage);
         }
         return null;
+    }
+
+    private void validateNicknameDuplication(String nickname){
+        Optional<Member> findMember = memberRepository.findByNickname(nickname);
+
+        if(findMember.isPresent())throw new IllegalStateException("ERR : "+nickname+"은 중복 닉네임 입니다");
+    }
+
+    private List<Genre> getValidGenres(List<String> genres) {
+        return genres.stream()
+                .map(genreName -> genreRepository.findByName(genreName)
+                        .orElseThrow(() -> new IllegalArgumentException("ERR : "+genreName+"은 존재하지 않는 장르입니다")))
+                .toList();
+    }
+
+    private AvatarType getAvatarType(String avatarTypeName) {
+        return avatarTypeRepository.findByName(avatarTypeName)
+                .orElseThrow(() -> new IllegalArgumentException("ERR : "+ avatarTypeName+"은 존재하지 않는 장르입니다"));
     }
 }
