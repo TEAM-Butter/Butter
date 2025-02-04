4a074776f9c888cd81aaa3370a2b4838dbc70669
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-04 17:56:15 +0900

    Feat : ChatService 생성 #S12P11E204-122

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/chat/service/ChatRedisService.java
@@ -0,0 +1,5 @@
+package com.ssafy.butter.domain.chat.service;
+
+public interface ChatRedisService {
+
+}
---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/chat/service/ChatRedisServiceImpl.java
@@ -0,0 +1,4 @@
+package com.ssafy.butter.domain.chat.service;
+
+public class ChatRedisServiceImpl implements ChatRedisService {
+}
