72120d4afecb9f99f2e1349d165129c9ea80d96a
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-05 10:16:34 +0900

    Comment : data-local 멤버 더미데이터 패스워드 주석 추가 #S12P11E204-122

--- backend/butter/src/main/resources/data-local.sql
+++ backend/butter/src/main/resources/data-local.sql
@@ -31,8 +31,9 @@ INSERT INTO avatar_type (name) VALUES
                                    ('Avatar6');
 
 -- member
+-- "password" : "mypassword1!"
 INSERT INTO member (member_type_id, avatar_type_id, login_id, nickname, email, birth_date, bread_amount, password, profile_image, gender, create_date, is_extra_info_registered)
-VALUES (1, 1, 'user123', 'User One', 'user1@example.com', '1990-01-01', 100, '$2a$12$dQo54URbmGg6L59qXSgLPOk/vuBg45UOfjSbY7MB/VanylMZhhHw2', NULL, 'MALE', NOW(), TRUE);
+VALUES (1, 1, 'user123', 'User One', 'user1@example.com', '1990-01-01', 100, '$2a$12$3MwPS4LQvrcCCYSzLpMV7usfb9oMYxoH7ByZGlwUB4BbvpitnLlrq', NULL, 'MALE', NOW(), TRUE);
 
 INSERT INTO member (member_type_id, avatar_type_id, login_id, nickname, email, birth_date, bread_amount, password, profile_image, gender, create_date, is_extra_info_registered)
 VALUES (2, 2, 'user456', 'User Two', 'user2@example.com', '1995-05-15', 200, '$2a$12$XoEzMrF9cwmKafmDaH62P.YcL554rfwGfghxNFH6.CmQZ0S7NUxRW', NULL, 'FEMALE', NOW(), FALSE);
