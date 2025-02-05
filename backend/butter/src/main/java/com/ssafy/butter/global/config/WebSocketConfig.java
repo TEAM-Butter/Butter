8caec3799638aeb758cc8d24ade91a9c1fd91ba4
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-06 00:50:12 +0900

    Feat: WebSocketConfig 수정 #S12P11E204-122

--- backend/butter/src/main/java/com/ssafy/butter/global/config/WebSocketConfig.java
+++ backend/butter/src/main/java/com/ssafy/butter/global/config/WebSocketConfig.java
@@ -1,8 +1,6 @@
 package com.ssafy.butter.global.config;
 
-import com.ssafy.butter.global.interceptor.StompInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -12,26 +10,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final StompInterceptor stompInterceptor;
-
-    public WebSocketConfig(StompInterceptor stompInterceptor) {
-        this.stompInterceptor = stompInterceptor;
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("ws-stomp").setAllowedOrigins("*")
-                .withSockJS();
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(stompInterceptor);
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
     }
 }
