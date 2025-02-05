124299865891a3c0a403fad25d5861763c292ee2
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-06 00:52:03 +0900

    Feat: 메시지 브로드캐스트 api 작성 #S12P11E204-122

--- backend/butter/src/main/java/com/ssafy/butter/domain/chat/controller/ChatController.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/chat/controller/ChatController.java
@@ -2,7 +2,10 @@ package com.ssafy.butter.domain.chat.controller;
 
 import com.ssafy.butter.domain.chat.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -10,10 +13,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    @PostMapping("/chat/message")
-    public void message (ChatMessage message){
+    @MessageMapping("/chat.sendMessage/{roomId}")
+    public void sendMessage(@DestinationVariable String roomId, ChatMessage chatMessage) {
+        // 동적으로 생성된 채팅방 "/topic/rooms/{roomId}" 토픽으로 메시지 브로드캐스트
+        messagingTemplate.convertAndSend("/topic/rooms/" + roomId, chatMessage);
+    }
 
+    @MessageMapping("/chat.addUser/{roomId}")
+    public void addUser(@DestinationVariable String roomId, ChatMessage chatMessage) {
+        messagingTemplate.convertAndSend("/topic/rooms/" + roomId, chatMessage);
     }
 }
