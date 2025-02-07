d28a63526137e39fecbb9ac1822d60fefcb23446
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-07 16:55:54 +0900

    Feat: 로그인 응답 DTO 수정

--- backend/butter/src/main/java/com/ssafy/butter/auth/dto/response/LoginResponseDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/auth/dto/response/LoginResponseDTO.java
@@ -1,10 +1,8 @@
 package com.ssafy.butter.auth.dto.response;
 
-import com.ssafy.butter.domain.member.dto.response.RegisterExtraInfoResponseDTO;
-
 public record LoginResponseDTO(
         String accessToken,
         String refreshToken,
-        RegisterExtraInfoResponseDTO extraInfo
+        AuthenticatedMemberInfoDTO authenticatedMemberInfo
 ) {
 }
