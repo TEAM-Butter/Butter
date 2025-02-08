da4085b6ee952b1dfcdbb54e0960d3fc84136e23
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-09 01:17:43 +0900

    Feat : 채팅 컨트롤러 구현

--- backend/butter/src/main/java/com/ssafy/butter/domain/chat/controller/ChatController.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/chat/controller/ChatController.java
@@ -1,28 +1,44 @@
 package com.ssafy.butter.domain.chat.controller;
 
 import com.ssafy.butter.domain.chat.dto.ChatMessage;
-import lombok.RequiredArgsConstructor;
+import com.ssafy.butter.domain.chat.dto.ChatMessage.MessageType;
+import com.ssafy.butter.domain.chat.handler.ChatMessageHandler;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
-@RequiredArgsConstructor
 public class ChatController {
 
-    private final SimpMessagingTemplate messagingTemplate;
+    private final Map<MessageType, ChatMessageHandler> handlerMap = new HashMap<>();
 
-    @MessageMapping("/chat.sendMessage/{roomId}")
-    public void sendMessage(@DestinationVariable String roomId, ChatMessage chatMessage) {
-        // 동적으로 생성된 채팅방 "/topic/rooms/{roomId}" 토픽으로 메시지 브로드캐스트
-        messagingTemplate.convertAndSend("/topic/rooms/" + roomId, chatMessage);
+    @Autowired
+    public ChatController(List<ChatMessageHandler> handlers){
+        for(ChatMessageHandler handler : handlers){
+            handlerMap.put(handler.getMessageType(), handler);
+        }
     }
 
-    @MessageMapping("/chat.addUser/{roomId}")
-    public void addUser(@DestinationVariable String roomId, ChatMessage chatMessage) {
-        messagingTemplate.convertAndSend("/topic/rooms/" + roomId, chatMessage);
+    @MessageMapping("/chat.sendMessage/{streamId}")
+    public void sendMessage(@DestinationVariable String streamId,
+                            ChatMessage message,
+                            StompHeaderAccessor headerAccessor) {
+
+        message.setStreamId(streamId);
+        ChatMessageHandler handler = handlerMap.get(message.getType());
+
+        if(handler != null){
+            handler.handleMessage(message, headerAccessor);
+            return;
+        }
+
+        System.err.println("ERR : 알 수 없는 메시지 타입 입니다" + message.getType());
     }
 }
