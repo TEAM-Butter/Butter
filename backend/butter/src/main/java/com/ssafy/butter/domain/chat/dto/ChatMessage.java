586b5757be9b5c7adc7e61429f2aa27172ee2718
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-09 01:14:30 +0900

    Feat : 채팅 메시지 DTO 구현 #S12P11E204-122

--- backend/butter/src/main/java/com/ssafy/butter/domain/chat/dto/ChatMessage.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/chat/dto/ChatMessage.java
@@ -1,16 +1,22 @@
 package com.ssafy.butter.domain.chat.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class ChatMessage {
     private MessageType type;
     private String content;
     private String sender;
+    private String streamId;
+    private String role;
 
     public enum MessageType {
         CHAT,
         JOIN,
-        LEAVE
+        LEAVE,
+        SYSTEM,
+        STREAM_ENDED
     }
 }
