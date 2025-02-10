95456e55dc5e2c5c9de17ac683427eecf1878f60
5almiakki <jpui9635@gmail.com>
2025-02-10 16:39:19 +0900

    Feat: 크루 목록 조회 메서드 repository에 추가 (미완) #S12P11E204-133

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/crew/CrewRepositoryImpl.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/crew/CrewRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.ssafy.butter.domain.crew.repository.crew;
 
-import com.ssafy.butter.domain.crew.entity.Crew;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.butter.domain.crew.dto.request.CrewListRequestDTO;
+import com.ssafy.butter.domain.crew.entity.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +16,10 @@ import java.util.Optional;
 @Repository
 public class CrewRepositoryImpl implements CrewRepository {
 
+    private static final QCrew qCrew = QCrew.crew;
+    private static final QGenre qGenre = QGenre.genre;
+
+    private final JPAQueryFactory jpaQueryFactory;
     private final CrewJpaRepository crewJpaRepository;
 
     @Override
@@ -37,5 +45,25 @@ public class CrewRepositoryImpl implements CrewRepository {
     @Override
     public List<Crew> findAllByIdLessThanOrderByIdDesc(Long id, Pageable pageable) {
         return crewJpaRepository.findAllByIdLessThanOrderByIdDesc(id, pageable);
+    }
+
+    @Override
+    public List<Crew> getCrewList(CrewListRequestDTO crewListRequestDTO) {
+        return jpaQueryFactory.selectFrom(qCrew)
+                .where(createCrewListCondition(crewListRequestDTO))
+                .limit(crewListRequestDTO.pageSize())
+                .fetch();
+    }
+
+    private BooleanExpression[] createCrewListCondition(CrewListRequestDTO crewListRequestDTO) {
+        String[] keywords = crewListRequestDTO.keyword().split(" ");
+        Genre genre = jpaQueryFactory.selectFrom(qGenre)
+                .innerJoin(qCrew)
+                .where(qGenre.name.eq(crewListRequestDTO.genre()))
+                .fetchOne();
+        return new BooleanExpression[] {
+                crewListRequestDTO.crewId() == null ? null : qCrew.id.lt(crewListRequestDTO.crewId()),
+                Arrays.stream(keywords).map(qCrew.name::contains).reduce(BooleanExpression::and).orElse(null),
+        };
     }
 }
