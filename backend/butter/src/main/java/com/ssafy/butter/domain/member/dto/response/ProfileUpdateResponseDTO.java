2f76e804b3ac6c04aa0b9c12659217f6b1d39274
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-10 14:52:54 +0900

    Fix : 프로필 수정 요청/응답 DTO 수정

--- backend/butter/src/main/java/com/ssafy/butter/domain/member/dto/response/ProfileUpdateResponseDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/member/dto/response/ProfileUpdateResponseDTO.java
@@ -1,14 +1,28 @@
 package com.ssafy.butter.domain.member.dto.response;
 
+import com.ssafy.butter.domain.crew.entity.Genre;
 import com.ssafy.butter.domain.member.entity.Member;
+import com.ssafy.butter.domain.member.entity.MemberGenre;
+
+import java.util.List;
 
 public record ProfileUpdateResponseDTO(
         Long id,
         String nickname,
-        String gender,
-        String profileImage
+        String profileImageUrl,
+        String avatarType,
+        List<String> genres
 ) {
-    public ProfileUpdateResponseDTO(Member member){
-        this(member.getId(), member.getNickname().getValue(), member.getGender().name(), member.getProfileImage());
+    public static ProfileUpdateResponseDTO from(Member member){
+        List<String> genres = member.getMemberGenres().stream()
+                .map(MemberGenre::getGenre)
+                .map(Genre::getName)
+                .toList();
+        return new ProfileUpdateResponseDTO(
+                member.getId(),
+                member.getNickname().getValue(),
+                member.getProfileImage(),
+                member.getAvatarType().getName(),
+                genres);
     }
 }
