088d99cb7d73a712ea9888a07185b70d5d24817c
5almiakki <jpui9635@gmail.com>
2025-02-11 15:27:59 +0900

    Feat: 버스킹 일정 조회 요청에 정렬 기준 포함 #S12P11E204-133

--- backend/butter/src/main/java/com/ssafy/butter/domain/schedule/dto/request/ScheduleSearchRequestDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/schedule/dto/request/ScheduleSearchRequestDTO.java
@@ -1,6 +1,21 @@
 package com.ssafy.butter.domain.schedule.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
-public record ScheduleSearchRequestDTO(Long scheduleId, Integer pageSize, String keyword, LocalDate date, Double latitude, Double longitude) {
+public record ScheduleSearchRequestDTO(
+
+        Long scheduleId,
+
+        @NotNull
+        Integer pageSize,
+        String keyword,
+        LocalDate date,
+        Double latitude,
+        Double longitude,
+
+        @NotNull
+        String sortBy
+) {
 }
