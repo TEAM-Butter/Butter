4602304279cdb52b7f78a4d366ff97d60256eedf
5almiakki <jpui9635@gmail.com>
2025-02-12 22:11:10 +0900

    Fix: API 경로에서 api 제거

--- backend/butter/src/main/java/com/ssafy/butter/domain/member/controller/MemberController.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/member/controller/MemberController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/members")
+@RequestMapping("/v1/members")
 @RequiredArgsConstructor
 @Tag(name = "Member Controller", description = "회원 관련 API")
 public class MemberController {
