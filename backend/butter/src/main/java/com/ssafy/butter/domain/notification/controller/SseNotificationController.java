3b96c769bdf4c93121de6b44f107fc3b0201ae3d
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-14 07:05:53 +0900

    Feat: 실시간 알림 컨트롤러 작성

---
+++ backend/butter/src/main/java/com/ssafy/butter/domain/notification/controller/SseNotificationController.java
@@ -0,0 +1,31 @@
+package com.ssafy.butter.domain.notification.controller;
+
+import com.ssafy.butter.auth.dto.AuthInfoDTO;
+import com.ssafy.butter.domain.notification.service.SseNotificationService;
+import com.ssafy.butter.global.token.CurrentUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/v1/notify")
+public class SseNotificationController {
+
+    private final SseNotificationService sseNotificationService;
+
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> subscribe(
+            @CurrentUser AuthInfoDTO authInfoDTO,
+            @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
+
+        SseEmitter sseEmitter = sseNotificationService.subscribe(authInfoDTO.id(), lastEventId);
+
+        return ResponseEntity.ok(sseEmitter);
+    }
+}
