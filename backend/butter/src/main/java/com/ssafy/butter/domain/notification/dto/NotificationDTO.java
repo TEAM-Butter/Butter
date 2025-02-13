7ac28ae06b515f7e274455b4ec63ac5191954f8e
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-14 07:04:36 +0900

    Feat: 실시간 알림 DTO 생성 #S12P11E204-183

--- backend/butter/src/main/java/com/ssafy/butter/domain/notification/dto/NotificationDTO.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/notification/dto/NotificationDTO.java
@@ -1,6 +1,5 @@
 package com.ssafy.butter.domain.notification.dto;
 
-import com.ssafy.butter.domain.notification.entity.Notification;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,21 +9,9 @@ public class NotificationDTO {
 
     private Long id;
     private String content;
-    private Long memberId;
+    private Long receiver;
     private String url;
     private String notificationType;
     private char readYn;
     private char deletedYn;
-
-    public static NotificationDTO fromEntity(Notification notification) {
-        return NotificationDTO.builder()
-                .id(notification.getId())
-                .content(notification.getContent())
-                .memberId(notification.getMember().getId())
-                .url(notification.getUrl())
-                .notificationType(notification.getNotificationType())
-                .readYn(notification.getReadYn())
-                .deletedYn(notification.getDeletedYn())
-                .build();
-    }
 }
