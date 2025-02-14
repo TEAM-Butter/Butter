b63f201614342e19c176706150421461325cc856
5almiakki <jpui9635@gmail.com>
2025-02-14 14:04:48 +0900

    Feat: 빵 로그 생성 로직 작성 #S12P11E204-193

--- backend/butter/src/main/java/com/ssafy/butter/domain/bread/service/BreadService.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/bread/service/BreadService.java
@@ -2,11 +2,13 @@ package com.ssafy.butter.domain.bread.service;
 
 import com.ssafy.butter.auth.dto.AuthInfoDTO;
 import com.ssafy.butter.domain.bread.dto.request.BreadDonationRequestDTO;
-import com.ssafy.butter.domain.bread.dto.request.BreadRechargeRequestDTO;
+import com.ssafy.butter.domain.bread.dto.request.BreadSettlementRequestDTO;
 
 public interface BreadService {
 
     void rechargeBread(AuthInfoDTO authInfoDTO, int amount);
 
     void donateBread(AuthInfoDTO authInfoDTO, BreadDonationRequestDTO breadDonationRequestDTO);
+
+    void settleBread(AuthInfoDTO authInfoDTO, BreadSettlementRequestDTO breadSettlementRequestDTO);
 }
--- backend/butter/src/main/java/com/ssafy/butter/domain/bread/service/BreadServiceImpl.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/bread/service/BreadServiceImpl.java
@@ -3,6 +3,11 @@ package com.ssafy.butter.domain.bread.service;
 import com.ssafy.butter.auth.dto.AuthInfoDTO;
 import com.ssafy.butter.domain.bread.dto.request.BreadDonationRequestDTO;
 import com.ssafy.butter.domain.bread.dto.request.BreadRechargeRequestDTO;
+import com.ssafy.butter.domain.bread.dto.request.BreadSettlementRequestDTO;
+import com.ssafy.butter.domain.bread.entity.BreadLog;
+import com.ssafy.butter.domain.bread.entity.BreadLogType;
+import com.ssafy.butter.domain.bread.repository.BreadLogRepository;
+import com.ssafy.butter.domain.bread.repository.BreadLogTypeRepository;
 import com.ssafy.butter.domain.crew.entity.Crew;
 import com.ssafy.butter.domain.crew.service.CrewService;
 import com.ssafy.butter.domain.member.entity.Member;
@@ -21,6 +26,9 @@ public class BreadServiceImpl implements BreadService {
     private final MemberService memberService;
     private final CrewService crewService;
 
+    private final BreadLogRepository breadLogRepository;
+    private final BreadLogTypeRepository breadLogTypeRepository;
+
     @Override
     public void rechargeBread(AuthInfoDTO authInfoDTO, int amount) {
         log.info("recharge bread: {}", amount);
@@ -28,6 +36,11 @@ public class BreadServiceImpl implements BreadService {
         if (member.getBreadAmount() == null) {
             member.updateBreadAmount(0);
         }
+        BreadLog breadLog = BreadLog.builder()
+                .member(member)
+                .breadLogType(breadLogTypeRepository.findByName("Recharge").orElseThrow())
+                .build();
+        breadLogRepository.save(breadLog);
         member.updateBreadAmount(member.getBreadAmount().getAmount() + amount);
     }
 
@@ -38,8 +51,19 @@ public class BreadServiceImpl implements BreadService {
             throw new IllegalArgumentException("Member does not have enough bread amount");
         }
         Crew crew = crewService.findById(breadDonationRequestDTO.crewId());
-
+        BreadLog breadLog = BreadLog.builder()
+                .member(member)
+                .crew(crew)
+                // TODO 리펙토링...?
+                .breadLogType(breadLogTypeRepository.findByName("Donate").orElseThrow())
+                .build();
+        breadLogRepository.save(breadLog);
         member.updateBreadAmount(member.getBreadAmount().getAmount() - breadDonationRequestDTO.amount());
         crew.updateDonationAmount(crew.getDonationAmount() + breadDonationRequestDTO.amount());
+    }
+
+    @Override
+    public void settleBread(AuthInfoDTO authInfoDTO, BreadSettlementRequestDTO breadSettlementRequestDTO) {
+
     }
 }
