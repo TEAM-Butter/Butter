82028b5c25b2e2ec6ddfc3681bef27712e205549
이병건 <rha1108@naver.com>
2025-02-14 17:53:38 +0900

    Test: LIVEKIT_URL 변경

--- backend/butter_webrtc/public/app.js
+++ backend/butter_webrtc/public/app.js
@@ -16,7 +16,7 @@ function configureLiveKitUrl() {
         } else {
             LIVEKIT_URL = "wss://" + window.location.hostname + ":7443/";
         }
-        //LIVEKIT_URL = "wss://i12e204.p.ssafy.io:5443";
+        LIVEKIT_URL = "wss://i12e204.p.ssafy.io:5443";
     }
 }
 
