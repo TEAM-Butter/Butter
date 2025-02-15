b341469b44cdef7658137aca3bc288f72dcfd5bb
dahee1029 <dlaekgml222@naver.com>
2025-02-15 18:16:41 +0900

    Feat: ClipPage 라우트 설정

--- frontend/butter/src/App.tsx
+++ frontend/butter/src/App.tsx
@@ -3,8 +3,8 @@ import NotFoundPage from "./pages/error/NotFoundPage";
 import { userRoutes } from "./routes/userRoutes";
 import { streamRoutes } from "./routes/streamRoutes";
 import { buskingRoutes } from "./routes/buskingRoutes";
-import { crewRoutes } from "./routes/crewRoutes"
-import { breadRoutes } from "./routes/breadRoutes"
+import { crewRoutes } from "./routes/crewRoutes";
+import { breadRoutes } from "./routes/breadRoutes";
 import { homeRoutes } from "./routes/homeRoutes";
 import "./styles/index.css";
 import { CommonLayout } from "./layouts/CommonLayout";
@@ -14,6 +14,7 @@ import "./styles/index.css";
 import "./styles/stream.css";
 import LiveListPage from "./pages/stream/LiveListPage";
 import VideoEditPage from "./pages/video/VideoEditPage";
+import { videoRoutes } from "./routes/videoRoutes";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           {buskingRoutes}
           {crewRoutes}
           {breadRoutes}
+          {videoRoutes}
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
