f6c00744d44a9158b27984dd63eb19e33a4fdfa9
5almiakki <jpui9635@gmail.com>
2025-02-16 23:43:17 +0900

    Feat: 방별 닉네임 관리 #S12P11E204-224

--- backend/butter_ai/app/services/websocket_room_service.py
+++ backend/butter_ai/app/services/websocket_room_service.py
@@ -1,4 +1,5 @@
 room_motions = {}
+room_nicknames = {}
 
 def increase_motion_count(room_id, motion):
     if room_id not in room_motions:
@@ -9,6 +10,16 @@ def increase_motion_count(room_id, motion):
     return room_motions[room_id]
 
 
+def put_nickname(room_id, nickname):
+    if room_id not in room_nicknames:
+        room_nicknames[room_id] = []
+    if nickname not in room_nicknames[room_id]:
+        room_nicknames[room_id].append(nickname)
+    return room_nicknames[room_id]
+
+
 def remove_room(room_id):
     if room_id in room_motions:
         del room_motions[room_id]
+    if room_id in room_nicknames:
+        del room_nicknames[room_id]
