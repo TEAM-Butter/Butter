59eff29ad0b0dea5f0aba52fb7174b5bdf015496
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-02-17 00:23:29 +0900

    Feat: 클립 좋아요 수 반환 쿼리 메서드 추가

--- backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/LikedClipJpaRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/clip/repository/LikedClipJpaRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.butter.domain.clip.repository;
 import com.ssafy.butter.domain.clip.entity.Clip;
 import com.ssafy.butter.domain.clip.entity.LikedClip;
 import com.ssafy.butter.domain.member.entity.Member;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface LikedClipJpaRepository extends JpaRepository<LikedClip, Long> {
 
     Optional<LikedClip> findByMemberAndClip(Member member, Clip clip);
+
+    long countLikedClipByIdAndIsLiked(Long id, @NotNull Boolean isLiked);
 }
