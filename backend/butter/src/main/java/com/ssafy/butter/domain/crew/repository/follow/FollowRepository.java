68f3306d4fbab45b1036e2b056bc155bb65424f7
5almiakki <jpui9635@gmail.com>
2025-02-17 17:36:25 +0900

    Feat: Repository에 크루의 팔로워 수 조회 메서드 추가

--- backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/follow/FollowRepository.java
+++ backend/butter/src/main/java/com/ssafy/butter/domain/crew/repository/follow/FollowRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.butter.domain.crew.repository.follow;
 import com.ssafy.butter.domain.crew.entity.Crew;
 import com.ssafy.butter.domain.crew.entity.Follow;
 import com.ssafy.butter.domain.member.entity.Member;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Optional;
 
@@ -13,4 +14,6 @@ public interface FollowRepository {
     void delete(Follow follow);
 
     Optional<Follow> findByCrewAndMember(Crew crew, Member member);
+
+    Long countFollowByCrewIdAndIsFollowed(Long crewId, @NotNull Boolean isFollowed);
 }
