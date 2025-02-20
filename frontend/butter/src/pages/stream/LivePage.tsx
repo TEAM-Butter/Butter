ed2b81d7ec1afe3fe731307f3e731654c5bfdc22
dahee1029 <dlaekgml222@naver.com>
2025-02-20 22:36:26 +0900

    Feat: 라이브 시청자 채팅방 넓이 수정

--- frontend/butter/src/pages/stream/LivePage.tsx
+++ frontend/butter/src/pages/stream/LivePage.tsx
@@ -150,16 +150,15 @@ const BackBtn = styled.div`
 
 const RightMiddle = styled.div`
   border-radius: 20px;
+  width: 100%;
   flex: 1;
-  height: 500px;
-  padding: 10px;
   display: flex;
   overflow: hidden;
 
   @media (max-width: 780px) {
     width: calc(50% - 4px);
     aspect-ratio: 16 / 9;
-    height: 300px;
+    /* height: 300px; */
     flex: none;
   }
 `;
@@ -167,6 +166,7 @@ const RightMiddle = styled.div`
 const RecordingContent = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
   height: calc(100% - 20px); /* padding 고려 */
 `;
 
@@ -177,7 +177,7 @@ const RecordingListContainer = styled.div`
   flex-direction: column;
   gap: 12px;
   overflow-y: auto;
-  padding-right: 8px;
+  width: 100%;
 
   /* Customize scrollbar */
   &::-webkit-scrollbar {
